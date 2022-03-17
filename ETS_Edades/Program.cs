da0d1053b3a78f3a1adddfffdd53536e72052cd3
Gisela FP <gisyestevezfp@gmail.com>
2022-03-17 13:22:53 +0000

    Agregar archivos de proyecto.

---
+++ ETS_Edades/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ETS_Edades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
