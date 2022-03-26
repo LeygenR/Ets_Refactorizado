0f4173c3091d8cbbfb6b964909d993c248a7a2f8
Mario <f23o11d2001@gmail.com>
2022-03-26 09:29:28 +0000

    Restructurado de funciones arreglando el problema de los años bisiestos para calcular la edad y cambios en el program.cs para recoger bien las dos fechas de las personas

--- ETS_Edades/Funciones.cs
+++ ETS_Edades/Funciones.cs
@@ -9,23 +9,24 @@ namespace ETS_Edades
     class Funciones
     {
         /// <summary>
-        /// Función para obetenr una fecha de nacimiento válida(Después de cristo)
+        /// Función para obtener una fecha correcta de nacimiento
         /// </summary>
-        /// <returns>Devuelve una fecha válida introducida por teclado</returns>
-        public static DateTime LeerFechaNacimiento()
+        /// <param name="contadorMostrar">contador para tener secuencia del pedido de fechas por persona</param>
+        /// <returns>Devuelve una fecha de nacimiento correcta(no supere los años actuales y contando los bisiestos cumpliendo el formato correcto)</returns>
+        public static DateTime LeerFechaNacimiento(int contadorMostrar)
         {
             DateTime fechaNacimiento = DateTime.Now;//la inicializo con la fecha actual para que no de errores
             bool leer = false;//booleano para salir solo cuando lea una fecha válida
             do
             {
-                Console.WriteLine("Introduzca la fecha de la persona en el formato correcto(dd/MM/yyyy, por ejemplo 01/01/2001)");
+                Console.WriteLine("Introduzca la fecha de la persona {0} en el formato correcto(dd/MM/yyyy, por ejemplo 01/01/2001)",contadorMostrar);
                 try
                 {
                     fechaNacimiento = DateTime.ParseExact(Console.ReadLine().Trim(), "dd/MM/yyyy", null);//En caso de error en la conversión del parse sale la excepción del catch
                     DateTime actual = DateTime.Now;//fecha actual para compararla a la introducida por teclado
                     if(actual.Year < fechaNacimiento.Year)//comparamos si la fecha introducida no supera a la actual
                     {
-                        Console.WriteLine("Error, la fecha introducida de la persona no es válida, aun no ha nacido");
+                        Console.WriteLine("Error, la fecha introducida de la persona no es válida, aún no ha nacido");
                     }
                     else
                     {
@@ -46,14 +47,32 @@ namespace ETS_Edades
         /// <param name="fechaNacimiento">fecha de la persona</param>
         /// <param name="dias">Días que tiene</param>
         /// <param name="anios">Edad actual</param>
-        public static void ObtenerDiasYAnios(DateTime fechaNacimiento,ref double dias,ref double anios)
+        public static double ObtenerDias(DateTime fechaNacimiento)
         {
+            double dias;
             DateTime fechaActual = DateTime.Now;
             TimeSpan resta = fechaActual - fechaNacimiento;//Creo un Timespan que calcula un periodo de tiempo en nanosegundos(100)
-            dias = resta.TotalDays;//uso la función que devulve los nanosegundos en días totales(como son exactos debe ser double la variable dias y luego hacer un Math.Floor al valor)
-            anios = Math.Floor(dias / 365);//aquí lo divido entre la cantidas de dias que tiene un año 
+            double diasExactos = resta.TotalDays;//uso la función que devulve los nanosegundos en días totales(como son exactos debe ser double la variable dias y luego hacer un Math.Floor al valor)
+            //anios = Math.Floor(dias / 365);aquí lo divido entre la cantidas de dias que tiene un año
+            dias = Math.Floor(diasExactos);
+            return dias;
         }
-
+        /// <summary>
+        /// Función para obtener los años de la persona
+        /// </summary>
+        /// <param name="fechaNacimiento">Fecha respectiva de la persona</param>
+        /// <returns>Edad en años</returns>
+        public static int ObtenerAnios(DateTime fechaNacimiento)
+        {
+            int anios;
+            DateTime fechaActual = DateTime.Now;
+            anios = fechaActual.Year - fechaNacimiento.Year;
+            if (fechaNacimiento > fechaActual.AddYears(-anios))//volver al año en que nació la persona en caso de bisiesto
+            {
+                anios--;
+            }
+            return anios;
+        }
 
     }
 }
--- ETS_Edades/Program.cs
+++ ETS_Edades/Program.cs
@@ -6,11 +6,45 @@ namespace ETS_Edades
     {
         static void Main(string[] args)
         {
-            DateTime FechaPersona1 = Funciones.LeerFechaNacimiento();
-            double dias = 0;
-            double anios = 0;
-            Funciones.ObtenerDiasYAnios(FechaPersona1,ref dias,ref anios);
-            Console.WriteLine("La fecha {0} tiene {1} dias y {2} años",FechaPersona1,Math.Floor(dias),anios);
+
+            int contadorMostrar = 1;
+            bool correcto = false;
+            DateTime FechaPersona1 = DateTime.Today;
+            double diasPersona1 = 0;
+            int aniosPersona1 = 0;
+            DateTime FechaPersona2 = DateTime.Today;
+            double diasPersona2 = 0;
+            int aniosPersona2 = 0;
+            while (!correcto)
+            {
+                FechaPersona1 = Funciones.LeerFechaNacimiento(contadorMostrar);
+                diasPersona1 = Funciones.ObtenerDias(FechaPersona1);
+                if(diasPersona1 <= 0)
+                {
+                    Console.WriteLine("Error, la persona aún no ha nacido\nDeberá introducir una nueva fecha correcta");
+                }
+                else
+                {
+                    aniosPersona1 = Funciones.ObtenerAnios(FechaPersona1);
+                    Console.WriteLine("La persona {0} tiene {1} dias y {2} años", contadorMostrar, diasPersona1, aniosPersona1);
+                    contadorMostrar++;
+                    while (!correcto)
+                    {
+                        FechaPersona2 = Funciones.LeerFechaNacimiento(contadorMostrar);
+                        diasPersona2 = Funciones.ObtenerDias(FechaPersona2);
+                        if (diasPersona2 <= 0)
+                        {
+                            Console.WriteLine("Error, la persona aún no ha nacido\nDeberá introducir una nueva fecha correcta");
+                        }
+                        else
+                        {
+                            aniosPersona2 = Funciones.ObtenerAnios(FechaPersona2);
+                            Console.WriteLine("La persona {0} tiene {1} dias y {2} años", contadorMostrar, diasPersona2, aniosPersona2);
+                            correcto = true;
+                        }
+                    }
+                }                
+            }
             Console.Read();
         }
     }
