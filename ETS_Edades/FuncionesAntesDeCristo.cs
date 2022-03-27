db3b39e0bfdd7a9172d9e697695138e2023d8f05
Mario <f23o11d2001@gmail.com>
2022-03-27 23:45:01 +0100

    solución error public class FuncionesAntesDecCristo

--- ETS_Edades/FuncionesAntesDeCristo.cs
+++ ETS_Edades/FuncionesAntesDeCristo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ETS_Edades
 {
-    class FuncionesAntesDeCristo
+    public class FuncionesAntesDeCristo
     {
         public static string[] LeerFechaNacimiento(int contadorMostrar)
         {
